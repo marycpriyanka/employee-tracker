f9f1b60363764b45e9fa16c26f6faab6dc47dcfb
Priyanka Mary Chrstine <marycpriyanka@gmail.com>
2022-03-08 16:21:53 -0800

    Design and pre populate database

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+        REFERENCES department(id)
+        ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+        REFERENCES role(id)
+        ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+        REFERENCES employee(id)
+        ON DELETE SET NULL
+);
